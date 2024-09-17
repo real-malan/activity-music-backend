4d82cf66a0262f11acfe95059187acdee6db5a1b
Michał Malanowicz <mmalanowicz@apreel.com>
2024-09-17 15:28:58 +0200

    feat: allow to link multiple services to account

--- src/main/kotlin/pl/activitymusic/backend/business/account/entity/Account.kt
+++ src/main/kotlin/pl/activitymusic/backend/business/account/entity/Account.kt
@@ -1,27 +1,10 @@
 package pl.activitymusic.backend.business.account.entity
 
-import jakarta.persistence.*
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 
-@Entity
-@EntityListeners(AuditingEntityListener::class)
-class Account {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-
-    @LastModifiedDate
-    val lastModifiedDate: Instant? = null
-
-    @CreatedDate
-    val createdDate: Instant? = null
-
-    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    private val _services: List<Service> = mutableListOf()
-
-    val services get() = _services.toList()
-}
+data class Account (
+    val id: Long,
+    val services: List<Service>,
+    val updatedAt: Instant,
+    val createdAt: Instant
+)
